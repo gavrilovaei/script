20b272eaa1ce6b4e86668cc2c878a815fadf8c73
gavrilovaei <gavrilova-e@yandex.ru>
2021-10-15 00:54:25 +0300

    7

--- mail.php
+++ mail.php
@@ -27,12 +27,12 @@
 <?php
 $fio = $_POST['fio'];
 $email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
+// $fio = htmlspecialchars($fio);
+// $email = htmlspecialchars($email);
+// $fio = urldecode($fio);
+// $email = urldecode($email);
+// $fio = trim($fio);
+// $email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
